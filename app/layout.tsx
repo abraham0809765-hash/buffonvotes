d8df36d77d653912cfbaac594529ffd010662a52
abraham0809765-hash <abraham0809765@gmail.com>
2025-09-14 13:11:04 +0100

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { Sora, Lexend } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Sora({
+  variable: "--font-geist",
+  subsets: ["latin"],
+});
+
+const geistMono = Lexend({
+  variable: "--font-geist",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Vote your favourite model",
+  description: "Choose and vote for your favourite model.",
+  robots: "noindex",
+  openGraph: {
+    type: "website",
+    url: "https://rebuzy.netlify.app/",
+    title: "Vote your favourite model",
+    description: "Choose and vote for your favourite model.",
+    images: ["https://rebuzy.netlify.app/man.jpeg"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@your_twitter",
+    title: "Vote your favourite model",
+    description: "Choose and vote for your favourite model.",
+    images: ["https://rebuzy.netlify.app/man.jpeg"],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
